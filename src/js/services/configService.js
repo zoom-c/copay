c5ad3025627a298bb2b6ccf469eff450eb8fa723
upgradeadvice <upgradeadvice@gmail.com>
2015-07-26 18:46:32 -0500

    Update bws url

--- src/js/services/configService.js
+++ src/js/services/configService.js
@@ -12,7 +12,7 @@ angular.module('copayApp.services').factory('configService', function(storageSer
 
     // Bitcore wallet service URL
     bws: {
-      url: 'http://192.168.1.110:3232/bws/api',
+      url: 'https://bws.zoomcoin.co/bws/api',
     },
 
     // wallet default config
